0d178f959fc3bcc4c117eebe9efee907ed0fa93d
Knika Dass <knikadass@gmail.com>
2020-07-23 23:55:20 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,4 @@
-#class Dog
- # def initialize(name, breed)
-   # @name = name
-    #@breed = breed 
-   #if breed = nil
-   #breed= "Mutt"
-  #end
- #end
-#end
+
 
 class Dog
   def initialize(name, breed = "Mutt")
